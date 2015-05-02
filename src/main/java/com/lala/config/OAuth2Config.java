5f16e86cd7fe6731ff83f25c419887ff3f811944
qq <you@example.com>
2015-05-02 04:00:20 -0700

    a

--- src/main/java/com/lala/config/OAuth2Config.java
+++ src/main/java/com/lala/config/OAuth2Config.java
@@ -1,10 +1,28 @@
 package com.lala.config;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class OAuth2Config
 {
-	public static final String CLIENT_ID = "";
+	public static final String CLIENT_ID = "4033219693";
 	
-	public static final String CLIENT_SECRET = "";
+	public static final String CLIENT_SECRET = "752f7c9fa96e08eb169fa2bec2d5e526";
 	
-	public static final String REGISTERED_REDIRECT_URI = "";
+	public static final String REGISTERED_REDIRECT_URI;
+	
+	static
+	{
+		String url = null;
+		
+		try 
+		{
+			url = URLEncoder.encode("http://tangshiyi.oschina.mopaas.com/sina/callback.do", "UTF-8");
+		} catch (UnsupportedEncodingException e)
+		{
+			e.printStackTrace();
+		}
+		
+		REGISTERED_REDIRECT_URI = url;
+	}
 }
--- src/main/java/com/lala/listener/ConfigListener.java
+++ src/main/java/com/lala/listener/ConfigListener.java
@@ -1,13 +1,19 @@
 package com.lala.listener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import com.lala.config.OAuth2Config;
 
 public class ConfigListener implements ServletContextListener
 {
 	public void contextInitialized(ServletContextEvent sce)
 	{
-		
+		ServletContext context = sce.getServletContext();
+		context.setAttribute("CLIENT_ID", OAuth2Config.CLIENT_ID);
+		context.setAttribute("CLIENT_SECRET", OAuth2Config.CLIENT_SECRET);
+		context.setAttribute("REGISTERED_REDIRECT_URI", OAuth2Config.REGISTERED_REDIRECT_URI);
 	}
 
 	public void contextDestroyed(ServletContextEvent sce) 
