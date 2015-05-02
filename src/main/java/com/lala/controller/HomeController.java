8cdb6027f63736442bdd527da26b8f5e18f24c65
qq <you@example.com>
2015-05-02 08:20:38 -0700

    baidu.com

--- src/main/java/com/lala/controller/HomeController.java
+++ src/main/java/com/lala/controller/HomeController.java
@@ -31,6 +31,10 @@ public class HomeController
 	
 	private String getPageDir(String ua)
 	{
+		if(ua == null)
+		{
+			return "";
+		}
 		if(ua.indexOf("iPhone") != -1 || ua.indexOf("Mobile") != -1 || ua.indexOf("iPad") != -1 || ua.indexOf("Android") != -1)
 		{
 			return "/m";
